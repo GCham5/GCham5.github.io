37eff3033f9db1841980ba75b7bfd06c60a3c74c
Georges Chamoun <gcham042@uottawa.ca>
2020-07-22 05:04:53 -0400

    Update TodaysBeats.js

--- SEG3125-Module8-Lab/visionvibes/src/components/TodaysBeats.js
+++ SEG3125-Module8-Lab/visionvibes/src/components/TodaysBeats.js
@@ -6,8 +6,10 @@ import langData from '../data/language'
 class TodaysBeats extends Component {
     state = {
         beats: beatsData,
-        lang: langData['lang']
+        // lang: langData['lang']
     }
+
+    lang = langData['lang'];
 
     // componentWillMount = () =>{
     //     console.log(this.state.lang)
@@ -16,26 +18,26 @@ class TodaysBeats extends Component {
     //         this.setState({
     //           lang: 'FR'
     //         })
-      
+
     //       } else {
     //         this.setState({
     //           lang: 'EN'
     //         })
-      
+
     //       }
     // }
 
     render() {
-        const reducedBeats = beatsData.slice(0,3)
-        console.log(this.state.lang)
+        const reducedBeats = beatsData.slice(0, 3)
+        console.log(this.lang)
 
         return (
-            
+
             <div className=" container-fluid">
-                <h1>{this.state.lang === "EN" ? "Today's Beats" : "Les beats d'aujourd'hui"}</h1>
+                <h1>{this.lang === "EN" ? "Today's Beats" : "Les beats d'aujourd'hui"}</h1>
                 <div className="container ">
                     <div className="row subSection">
-                        <BeatCard beats={reducedBeats}/>
+                        <BeatCard beats={reducedBeats} />
                     </div>
                 </div>
             </div>
